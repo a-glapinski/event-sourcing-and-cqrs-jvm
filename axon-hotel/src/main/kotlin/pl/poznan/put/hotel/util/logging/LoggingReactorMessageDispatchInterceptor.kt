4208dd2979a1e8e4926b1077fea4efdd93f7cfc1
Adrian Glapiński <adriangl1997@gmail.com>
2022-06-03 23:48:22 +0200

    Add reactive web configuration

---
+++ axon-hotel/src/main/kotlin/pl/poznan/put/hotel/util/logging/LoggingReactorMessageDispatchInterceptor.kt
@@ -0,0 +1,15 @@
+package pl.poznan.put.hotel.util.logging
+
+import mu.KotlinLogging
+import org.axonframework.extensions.reactor.messaging.ReactorMessageDispatchInterceptor
+import org.axonframework.messaging.Message
+import reactor.core.publisher.Mono
+
+class LoggingReactorMessageDispatchInterceptor<M : Message<*>> : ReactorMessageDispatchInterceptor<M> {
+    private val logger = KotlinLogging.logger {}
+
+    override fun intercept(monoMessage: Mono<M>): Mono<M> =
+        monoMessage.doOnNext { message ->
+            logger.info("Dispatched message: [{}]", message.payloadType.simpleName)
+        }
+}
