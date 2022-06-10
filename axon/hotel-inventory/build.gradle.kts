5bc1b4d39b1219943ee7510195d464b59a3c4ce4
Adrian Glapiński <adriangl1997@gmail.com>
2022-06-10 23:17:12 +0200

    Use Spring reactive web dependencies

--- axon/hotel-inventory/build.gradle.kts
+++ axon/hotel-inventory/build.gradle.kts
@@ -17,28 +17,30 @@ repositories {
 }
 
 ext {
-	set("axonVersion", "4.5.12")
+	set("axonVersion", "4.5.13")
 }
 
 dependencies {
 	implementation(project(":axon:util"))
 	implementation("org.springframework.boot:spring-boot-starter-integration")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.axonframework:axon-spring-boot-starter")
 	implementation("org.axonframework.extensions.reactor:axon-reactor-spring-boot-starter")
 	implementation("org.axonframework.extensions.kotlin:axon-kotlin")
 	implementation("org.axonframework.extensions.tracing:axon-tracing-spring-boot-starter")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.6")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
-	implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.9")
 	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.axonframework:axon-test")
+	testImplementation("io.projectreactor:reactor-test")
 }
 
 dependencyManagement {
